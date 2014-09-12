bd18123d6d39a860ee095c835066226f480a0179
Harrison Wittke <16hawittke@gmail.com>
2014-09-13 00:35:38 +1000

    Sync 1
    Initial Files

---
+++ Phrase Generator/Form1.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnGenerate = New System.Windows.Forms.Button()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.picSettings = New System.Windows.Forms.PictureBox()
+        CType(Me.picSettings, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.Location = New System.Drawing.Point(359, 474)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(142, 23)
+        Me.btnGenerate.TabIndex = 0
+        Me.btnGenerate.Text = "Generate Phrase"
+        Me.btnGenerate.UseVisualStyleBackColor = True
+        '
+        'lblOutput
+        '
+        Me.lblOutput.AutoSize = True
+        Me.lblOutput.Font = New System.Drawing.Font("Impact", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOutput.Location = New System.Drawing.Point(303, 236)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(87, 35)
+        Me.lblOutput.TabIndex = 1
+        Me.lblOutput.Text = "Label1"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(732, 677)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(77, 17)
+        Me.LinkLabel1.TabIndex = 3
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "LinkLabel1"
+        '
+        'picSettings
+        '
+        Me.picSettings.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.picSettings.Image = Global.WindowsApplication1.My.Resources.Resources.settingson
+        Me.picSettings.Location = New System.Drawing.Point(65, 572)
+        Me.picSettings.Name = "picSettings"
+        Me.picSettings.Size = New System.Drawing.Size(20, 20)
+        Me.picSettings.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.picSettings.TabIndex = 4
+        Me.picSettings.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1234, 727)
+        Me.Controls.Add(Me.picSettings)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.btnGenerate)
+        Me.Name = "Form1"
+        Me.Text = "Phrase Generator"
+        CType(Me.picSettings, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnGenerate As System.Windows.Forms.Button
+    Friend WithEvents lblOutput As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents picSettings As System.Windows.Forms.PictureBox
+
+End Class
---
+++ Phrase Generator/Form1.vb
@@ -0,0 +1,103 @@
+﻿Public Class Form1
+    Dim input As String
+    Dim output As String
+    Dim phrases(200) As String
+    Dim nouns(200) As String
+    Dim verbs(200) As String
+    Dim adjectives(200) As String
+    Dim names(200) As String
+    Dim index As Integer
+    Dim rnd1 As Integer
+    Dim rnd2 As Integer
+    Dim rnd3 As Integer
+
+    Dim txtY As Integer = 10
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        populateArrays()
+        lblOutput.Text = String.Empty
+    End Sub
+
+    Private Sub btnGenerate_Click(sender As System.Object, e As System.EventArgs) Handles btnGenerate.Click
+        randomize()
+        output = "Well " & verbs(rnd1) & " me " & adjectives(rnd2) & " and call me " & names(rnd3) & "."
+        lblOutput.Text = output
+        'newTextbox()
+    End Sub
+
+    'USER DEFINES SUBS
+    Public Sub randomize()
+        rnd1 = Int(Rnd() * 10)
+        rnd2 = Int(Rnd() * 10)
+        rnd3 = Int(Rnd() * 10)
+    End Sub
+
+    Public Sub populateArrays()
+        verbs(0) = "kill"
+        verbs(1) = "lick"
+        verbs(2) = "slap"
+        verbs(3) = "tickle"
+        verbs(4) = "bash"
+        verbs(5) = "throw"
+        verbs(6) = "digest"
+        verbs(7) = "hit"
+        verbs(8) = "smash"
+        verbs(9) = "love"
+        verbs(10) = "kiss"
+
+        adjectives(0) = "sexy"
+        adjectives(1) = "silly"
+        adjectives(2) = "funny"
+        adjectives(3) = "blind"
+        adjectives(4) = "itchy"
+        adjectives(5) = "jealous"
+        adjectives(6) = "nervous"
+        adjectives(7) = "lazy"
+        adjectives(8) = "scary"
+        adjectives(9) = "grumpy"
+        adjectives(10) = "fierce"
+
+        names(0) = "Rowan"
+        names(1) = "Sally"
+        names(2) = "Suzy"
+        names(3) = "Sam"
+        names(4) = "Robert"
+        names(5) = "Timothy"
+        names(6) = "Clara"
+        names(7) = "Dwayne"
+        names(8) = "Renaldo"
+        names(9) = "Nila"
+        names(10) = "Chung"
+    End Sub
+
+    Public Sub newTextbox()
+        Dim f As New System.Drawing.Font("Sans Serif", 10)
+
+        Dim txtBox As New TextBox
+        txtBox.Width = 1000
+        txtBox.Height = 25
+        txtBox.Top = txtY
+        txtBox.Left = 850
+        txtBox.ReadOnly = True
+        txtBox.BorderStyle = BorderStyle.None
+        txtBox.Font = f
+        txtBox.Font = New System.Drawing.Font(txtBox.Font, FontStyle.Bold)
+        Me.Controls.Add(txtBox)
+        txtY = txtY + 30
+        txtBox.Text = output
+    End Sub
+
+    Private Sub picSettings_HoverOn(sender As System.Object, e As System.EventArgs) Handles picSettings.MouseEnter
+        picSettings.Image = My.Resources.settings
+    End Sub
+
+    Private Sub picSettings_HoverOff(sender As System.Object, e As System.EventArgs) Handles picSettings.MouseLeave
+        picSettings.Image = My.Resources.settingson
+    End Sub
+
+    Private Sub picSettings_Click(sender As System.Object, e As System.EventArgs) Handles picSettings.Click
+        Form2.Show()
+        Form2.Left = Me.Left
+        Form2.Top = Me.Top
+    End Sub
+End Class
